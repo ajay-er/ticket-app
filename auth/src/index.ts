9b3825ebee75bd617a75afe922f7a743baacc254
ajay <ajayzyan100@gmail.com>
2023-09-14 13:57:11 +0530

    ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/api/users/currentUser',(req,res)=>{
+  res.send('hey...how do you do?')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
