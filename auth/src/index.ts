031771e07ab368e73b42be218b6bc0d552c3efba
ajay <ajayzyan100@gmail.com>
2023-09-15 08:03:58 +0530

    mongoose connected

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from "./middlewares/error-handler";
+import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
-
 
 const app = express();
 app.use(express.json());
@@ -23,6 +23,17 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected To MongoDB');
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
+
+start();
