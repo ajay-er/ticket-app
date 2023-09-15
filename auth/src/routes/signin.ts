4dd893c82e9f5690d168ff91ad3ebfceff1328e7
ajay <ajayzyan100@gmail.com>
2023-09-15 21:06:35 +0530

    all routes updated

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,54 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
+
 
 const router = express.Router();
 
-router.get('/api/users/signin', (req, res) => {
-    res.send('hey...how do you do?');
-  });
+router.get(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password field cant be empty!'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if(!passwordsMatch){
+      throw new BadRequestError('Invalid credentials');
+    }
+    
+    const userJWT = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { jwt: userJWT };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('hey...how do you do?');
-  });
+  req.session = null;
+  res.send({ message: 'logout succefully' });
+});
 
 export { router as signoutRouter };
