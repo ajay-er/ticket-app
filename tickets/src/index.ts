d0ea96f01971f5fd470e53a61b9b068ec28bb6e5
ajay <ajayzyan100@gmail.com>
2023-10-06 21:26:48 +0530

    tickets service included

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not found');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI not found');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected To MongoDB');
+  } catch (error) {
+    console.log(error);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
+
+start();
