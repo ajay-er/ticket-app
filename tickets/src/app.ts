150860d9b2eddf3e5cb82a92224a71e161a01147
ajay <ajayzyan100@gmail.com>
2023-10-07 10:33:01 +0530

    tickets model, routes added

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { NotFoundError, errorHandler } from '@ajay404/common';
-
-
+import { NotFoundError, currentUser, errorHandler } from '@ajay404/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +14,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -31,19 +32,24 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  //build a JWT payload. { id,email}
+  const payload = {
+    id: '1234567890',
+    email: 'ajay@gmail.com',
+  };
+  //create jwt
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
+  //build session object
+  const session = { jwt: token };
 
-  const cookie = response.get('Set-Cookie');
+  //turn that session into json
+  const sessionJSON = JSON.stringify(session);
 
-  return cookie;
+  //take json and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  //return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
