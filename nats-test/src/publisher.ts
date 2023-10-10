372ab3b1c0285c90d962a71d248342257e20a60b
ajay <ajayzyan100@gmail.com>
2023-10-10 11:47:57 +0530

    publisher abstract class added

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 const client = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
@@ -6,16 +7,28 @@ const client = nats.connect('ticketing', 'abc', {
 
 console.clear();
 
-client.on('connect', () => {
+client.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(client);
 
-  client.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      price: 99,
+      title: 'hei_title_sample',
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // client.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
